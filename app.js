2a9c5646bfe208cb0464f7d0ded0b8453ab0304b
amanyty <amanyty@gmail.com>
2026-07-16 18:48:46 +0530

    Revert to Option 1: use perfectly readable OpenCV maps

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ const villageConfig = {
   baskhedi: {
     name: "Baskhedi",
     center: [75.2644, 24.2589],
-    imageOverlayUrl: "/baskhedi_ai.png?v=7",
+    imageOverlayUrl: "/baskhedi_ultra.png?v=8",
     imageCoordinates: [
       [75.2595, 24.2634], // top-left
       [75.2693, 24.2634], // top-right
@@ -42,7 +42,7 @@ const villageConfig = {
   village2: {
     name: "Junapani (Map 2)",
     center: [74.6942, 21.6899],
-    imageOverlayUrl: "/map2_ai.png?v=7", 
+    imageOverlayUrl: "/map2_enhanced.jpeg?v=3", 
     imageCoordinates: [
       [74.6842, 21.7049], // top-left
       [74.7042, 21.7049], // top-right
